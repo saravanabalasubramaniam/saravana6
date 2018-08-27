8f1b1ce021ba644f298a25186c78fcc77f99c8a6
saravana <saravanasaru14313@gmail.com>
2018-08-27 12:39:04 +0530

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class leapyear
+{
+public static void main(String[] args)
+{
+int year=2016;
+if(year%4==0)
+{
+if(year%400==0)
+{
+if(year%100!=0)
+{
+System.out.println("the year 2016 is leap year");
+}}}
+else
+{
+System.out.println("the year 2016 is not a leap year");
+}}}
