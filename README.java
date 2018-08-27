10c877ea861be340a78b205c54b51c4f0af9aee5
saravana <saravanasaru14313@gmail.com>
2018-08-27 13:45:35 +0530

    Update README.java

--- README.java
+++ README.java
@@ -1,19 +1,41 @@
 import java.io.*;
+
 import java.util.*;
-class leapyear
+
+class test
+
 {
-public static void main(String[] args)
+
+public static void main(String args[])
+
 {
+
 int year=2016;
+
+
+if((year%100)!=0)
+{
 if(year%4==0)
 {
 if(year%400==0)
 {
-if(year%100!=0)
-{
-System.out.println("the year 2016 is leap year");
-}}}
+System.out.println("Leap year");
+}
 else
 {
-System.out.println("the year 2016 is not a leap year");
-}}}
+System.out.println("Not a Leap year");
+}
+}
+else
+{
+System.out.println("Not a Leap year");
+}
+}
+else
+{
+System.out.println("Not a Leap year");
+}
+}
+}
+
+
